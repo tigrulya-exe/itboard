b1a294ac16269e5ccae01997036d570c173573a1
Tigran Manasyan <manasyan_tigran@mail.ru>
2019-12-30 00:37:52 +0700

    + user subscribers serialization handler was changed

--- src/main/java/ru/nsu/itboard/models/User.java
+++ src/main/java/ru/nsu/itboard/models/User.java
@@ -1,7 +1,15 @@
 package ru.nsu.itboard.models;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import ru.nsu.itboard.util.Converter;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -12,8 +20,10 @@ public class User {
 
     private String name;
 
+    @JsonSerialize(using = CustomSerializer.class)
     private Map<String, User> subscribers = new HashMap<>();
 
+    @JsonSerialize(using = CustomSerializer.class)
     private Map<String, User> subscriptions = new HashMap<>();
 
     private boolean isPrivateProfile;
@@ -25,5 +35,12 @@ public class User {
     public void generateId(){
         id = UUID.randomUUID().toString();
     }
+}
 
+class CustomSerializer extends JsonSerializer<Map<Long, User>> {
+    @Override
+    public void serialize(final Map<Long, User> value, final JsonGenerator jgen, final SerializerProvider provider)
+            throws IOException, JsonProcessingException {
+        jgen.writeObject(Converter.toUserTOs(value.values()));
+    }
 }
