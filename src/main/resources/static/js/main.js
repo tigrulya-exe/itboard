7c4ef002e24f22e8bcb7757a2606395be4a86743
Egor <egoridze81@gmail.com>
2019-12-29 12:58:02 +0700

    search page refactoring

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,27 +1,58 @@
+const searchPageTemplate = `
+<div class="row justify-content-center">
+          <form class="col-md-6" id = "searchForm">
+            <div class="row">
+              <input type="text" placeholder="Введите параметры поиска" class = "form-control mb-2" id = "searchInput">
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <p>Поиск по: </p>
+                <div class="row">
+                  <div class="col-md-6">
+                    <input type="radio" class = "form-check-input" checked name = "category" value = "users">
+                    <label class="form-check-label">
+                        Пользователям:
+                    </label>
+                  </div>
+                  <div class="col-md-3">
+                    <input type="radio" class = "form-check-input" name = "category" value = "events">
+                    <label class="form-check-label">
+                        Мероприятиям:
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button class="btn btn-primary mt-3" type="submit">Искать</button>
+          </form>
+        </div>
+`;
+
 function devideToSubarrays(array) {
     let size = 3;
     let subarray = [];
 
-    for (let i = 0; i <Math.ceil(array.length/size); i++){
-        subarray[i] = array.slice((i*size), (i*size) + size);
+    for (let i = 0; i < Math.ceil(array.length / size); i++) {
+        subarray[i] = array.slice(i * size, i * size + size);
     }
 
     return subarray;
 }
 
 function addListeners() {
-    document.querySelector('#getEvents')
-        .addEventListener('click', getAllEvents);
+    document
+        .querySelector("#getEvents")
+        .addEventListener("click", getAllEvents);
 
-    document.querySelector('#search')
-        .addEventListener('click', showSearchPage);
+    document.querySelector("#search").addEventListener("click", showSearchPage);
 
-    document.querySelector('#showProfile')
-        .addEventListener('click', showProfileInfo);
+    document
+        .querySelector("#showProfile")
+        .addEventListener("click", showProfileInfo);
 }
 
 async function getAllEvents() {
-    const url = '/events';
+    const url = "/events";
 
     const response = await fetch(url);
     const events = await response.json();
@@ -32,47 +63,46 @@ async function getAllEvents() {
 }
 
 function drawEvents(events) {
-    const pageContent = document.querySelector('#page-content');
-    pageContent.innerHTML = '';
+    const pageContent = document.querySelector("#page-content");
+    pageContent.innerHTML = "";
 
     const zippedEvents = devideToSubarrays(events);
 
     zippedEvents.forEach(pack => {
-        const row = document.createElement('div');
-        row.classList.add('row');
+        const row = document.createElement("div");
+        row.classList.add("row");
 
         pack.forEach(event => {
-            const card = document.createElement('div');
-            card.classList.add('col-md-3', 'card')
+            const card = document.createElement("div");
+            card.classList.add("col-md-3", "card");
 
             const eventTemplate = createCardTemplate(event);
             card.innerHTML = eventTemplate;
 
-            const followButton = document.createElement('button');
-            followButton.classList.add('btn', 'btn-primary');
+            const followButton = document.createElement("button");
+            followButton.classList.add("btn", "btn-primary");
             followButton.dataset.id = event.id;
-            followButton.addEventListener('click', subscribeToEvent);
-            followButton.textContent = 'Присоедениться';
+            followButton.addEventListener("click", subscribeToEvent);
+            followButton.textContent = "Присоедениться";
 
             card.appendChild(followButton);
             row.appendChild(card);
         });
 
         pageContent.appendChild(row);
-    })
+    });
 }
 
 async function subscribeToEvent() {
-    const userId = sessionStorage.getItem('id');
+    const userId = sessionStorage.getItem("id");
     const eventId = this.dataset.id;
     const url = `/events/${eventId}/${userId}`;
 
-    const response = await fetch(url, {method: 'POST'});
+    const response = await fetch(url, { method: "POST" });
 
     if (response.status == 200 || response.status == 201)
-        alert('Вы подписаны на событие!');
-    else
-        alert('Произшла ошибка!');
+        alert("Вы подписаны на событие!");
+    else alert("Произшла ошибка!");
 }
 
 function createCardTemplate(event) {
@@ -82,22 +112,32 @@ function createCardTemplate(event) {
     <p>Дата: ${event.beginDate}</p>
     <p>Продолжительность: ${event.duration}</p>
     <p>Описание: ${event.description}</p>
-    `
+    `;
 }
 
 function showSearchPage() {
-    const pageContent = document.querySelector('#page-content');
-    
-    pageContent.textContent = `Макет поисковой хрени`;
+    const pageContent = document.querySelector("#page-content");
+    pageContent.innerHTML = searchPageTemplate;
+
+    const searchForm = document.querySelector("#searchForm");
+    searchForm.addEventListener("submit", search);
+}
+
+function search(event) {
+    event.preventDefault();
+
+    const searchValue = document.querySelector("#searchInput").value;
+    const searchCategoty = Object.fromEntries(new FormData(event.target).entries()).category;
+    console.log(searchCategoty)
 }
 
 function showProfileInfo() {
-    const pageContent = document.querySelector('#page-content');
-    
+    const pageContent = document.querySelector("#page-content");
+
     pageContent.textContent = `Макет профиля`;
 }
 
 window.onload = () => {
     addListeners();
     getAllEvents();
-}
+};
